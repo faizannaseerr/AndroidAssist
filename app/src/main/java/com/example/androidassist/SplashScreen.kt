e16c7645bcdc6881129f1ecf56c4b664cb59172f
Asad <hasad514@gmail.com>
2024-02-24 14:54:11 -0500

    added splash screen

---
+++ app/src/main/java/com/example/androidassist/SplashScreen.kt
@@ -0,0 +1,32 @@
+package com.example.androidassist
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import androidx.annotation.RequiresApi
+
+class SplashScreen : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.decorView.windowInsetsController?.hide(WindowInsets.Type.statusBars())
+        window.decorView.windowInsetsController?.hide(WindowInsets.Type.navigationBars())
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        //Normal Handler is deprecated , so we have to change the code little bit
+
+        // Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3500) // 3000 is the delayed time in milliseconds.
+    }
+}
